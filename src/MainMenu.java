46082a2cc81dbef31bf0128d53667d20ae6921f8
aayushks-bu <aayushks@bu.edu>
2025-10-02 18:44:24 -0400

    adding files

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,4 +1,5 @@
-import java.util.Scanner; // Source: google
+import java.util.Scanner;
+
 public class MainMenu {
     public static void main(String[] args) {
         System.out.println("Welcome to Board Games!");
